de4a707d6beb52f3463805f90e55d069dc781ced
Josh Davenport-Smith <josh.davenport@tghp.co.uk>
2022-09-14 01:35:04 +0100

    Added type definitions

---
+++ index.d.ts
@@ -0,0 +1,26 @@
+export type ComponentFunction<args extends Object> = (args: args, el: Element) => void;
+
+declare class Components {
+    getInitAttributeName(): string;
+    getElementInstanceAttributeName(): string;
+    add(key: string, fn: Function): ThisType;
+    addInstance(el: Element, elementIdentifier: string, componentKey: string, fn: Function): ThisType;
+    getInstance(elementIdentifier: string, componentKey: string): ThisType;
+    getInstances(componentKey): [Element];
+    run(): void;
+}
+
+declare class Includes {
+    add(key: string, fn: Function): ThisType;
+    run(): ThisType;
+}
+
+declare class Groundwork {
+    constructor (namespace: string): ThisType;
+    run(): void;
+
+    components: Components;
+    includes: Includes;
+}
+
+export default Groundwork;
