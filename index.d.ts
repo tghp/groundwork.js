ac16d6a5b1bce67dd32821eede5d490d8d35e970
Josh Davenport-Smith <josh.davenport@tghp.co.uk>
2022-09-25 14:25:30 +0100

    Corrected types

--- index.d.ts
+++ index.d.ts
@@ -3,20 +3,20 @@ export type ComponentFunction<args extends Object> = (elem: Element, args: args)
 declare class Components {
     getInitAttributeName(): string;
     getElementInstanceAttributeName(): string;
-    add(key: string, fn: Function): ThisType;
-    addInstance(el: Element, elementIdentifier: string, componentKey: string, fn: Function): ThisType;
-    getInstance(elementIdentifier: string, componentKey: string): ThisType;
+    add(key: string, fn: Function): this;
+    addInstance(el: HTMLElement, elementIdentifier: string, componentKey: string, fn: Function): this;
+    getInstance(elementIdentifier: string, componentKey: string): this;
     getInstances(componentKey): [Element];
     run(): void;
 }
 
 declare class Includes {
-    add(key: string, fn: Function): ThisType;
-    run(): ThisType;
+    add(key: string, fn: Function): this;
+    run(): this;
 }
 
 declare class Groundwork {
-    constructor (namespace: string): ThisType;
+    constructor (namespace: string);
     run(): void;
 
     components: Components;
