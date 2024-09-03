c28a9dbdb0c2df54b4fd093b5779ad8816484764
Josh Davenport-Smith <josh.davenport@tghp.co.uk>
2024-09-03 21:48:35 +0100

    Improved ComponentFunction type

--- index.d.ts
+++ index.d.ts
@@ -1,26 +1,34 @@
-export type ComponentFunction<args extends Object> = (elem: HTMLElement, args: args) => void;
+export type ComponentFunction<
+  TElem extends HTMLElement,
+  TArgs extends Object
+> = (elem: TElem, args: TArgs) => void;
 
 declare class Components {
-    getInitAttributeName(): string;
-    getElementInstanceAttributeName(): string;
-    add(key: string, fn: Function): this;
-    addInstance(el: HTMLElement, elementIdentifier: string, componentKey: string, fn: Function): this;
-    getInstance(elementIdentifier: string, componentKey: string): this;
-    getInstances(componentKey): [HTMLElement];
-    run(): void;
+  getInitAttributeName(): string;
+  getElementInstanceAttributeName(): string;
+  add(key: string, fn: Function): this;
+  addInstance(
+    el: HTMLElement,
+    elementIdentifier: string,
+    componentKey: string,
+    fn: Function
+  ): this;
+  getInstance(elementIdentifier: string, componentKey: string): this;
+  getInstances(componentKey): [HTMLElement];
+  run(): void;
 }
 
 declare class Includes {
-    add(key: string, fn: Function): this;
-    run(): this;
+  add(key: string, fn: Function): this;
+  run(): this;
 }
 
 declare class Groundwork {
-    constructor (namespace: string);
-    run(): void;
+  constructor(namespace: string);
+  run(): void;
 
-    components: Components;
-    includes: Includes;
+  components: Components;
+  includes: Includes;
 }
 
 export default Groundwork;
